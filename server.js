e0a6ad1ac98f0d2117ef3d0b57c9c74773c58382
Salomon NSSO <salomonnssow10@gmail.com>
2021-10-25 16:50:20 +0100

    add freecodecamp additional constraints.

--- server.js
+++ server.js
@@ -15,37 +15,49 @@ app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+    res.sendFile(__dirname + '/views/index.html');
 });
 
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+    res.json({greeting: 'hello API'});
 });
 
 app.get("/api/:date", function (req, res) {
-  const {date: dateTimeStamp} = req.params
-  let result;
-  let d;
-  if(dateTimeStamp.split("-").length >= 3){
-    d = new Date(dateTimeStamp);
-    result = {
-      unix: d.getTime(),
-      utc: d.toUTCString()
-    }
-  } else {
-    d = new Date(+dateTimeStamp)
-    result = {
-      unix: d.getTime(),
-      utc: d.toUTCString()
-    }
-  }
+    const {date: dateTimeStamp} = req.params
 
-  res.json(result)
+    let result = {error : "Invalid Date"};
+    let d;
+
+      if (!dateTimeStamp.trim().length) {
+        d = new Date();
+        result = {
+          unix: d.getTime(),
+          utc: d.toUTCString()
+        }
+      } else if (dateTimeStamp.split("-").length >= 3) {
+        d = new Date(dateTimeStamp);
+        if(!isNaN(d)){
+          result = {
+            unix: d.getTime(),
+            utc: d.toUTCString()
+          }
+        }
+      } else {
+        d = new Date(+dateTimeStamp)
+        if(!isNaN(d)){
+          result = {
+            unix: d.getTime(),
+            utc: d.toUTCString()
+          }
+        }
+      }
+
+    res.json(result)
 });
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+    console.log('Your app is listening on port ' + listener.address().port);
 });
